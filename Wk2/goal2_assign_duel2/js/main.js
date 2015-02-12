bc385c70cda87f741d017f2be8cfb05b67ac8b7f
frniners <jerrysutton@Jerrys-MacBook-Pro.local>
2015-02-11 20:24:56 -0700

    update

---
+++ Wk2/goal2_assign_duel2/js/main.js
@@ -0,0 +1,88 @@
+/*
+ Name: Jerry Sutton
+ Date: 02/07/15
+ Assignment: Goal1: Assignment: Duel 1
+ */
+
+//self executing function
+
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Kabal";
+    var playerTwoName = "Kratos";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    var round=0;
+
+    function fight(){
+        console.log('in the fight function');
+
+        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+";"+playerTwoHealth);
+
+        for (var i=0; i < 10; i++) {
+            //random formula is - Math.floor(Math.random() * (max - min) + min);
+
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            //console.log(f1);
+            //console.log(f2);
+
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            //console.log(playerOneHealth);
+            //console.log(playerTwoHealth);
+
+            alert(playerOneName+":"+playerOneHealth+" "+playerTwoName+";"+playerTwoHealth);
+
+         var results = winnerCheck();
+         console.log(results);
+
+        if(results === "no winner"){
+        round++;
+        alert(playerOneName+":"+playerOneHealth+" *ROUND* "+round+" OVER* "+playerTwoName+";"+playerTwoHealth);
+        } else{
+          alert(results);
+            break;
+        };
+
+        };
+    };
+
+    function winnerCheck() {
+        console.log("in winnerCheck FN");
+
+        var result = "no winner";
+
+        if (playerOneHealth < 1 && playerTwoHealth < 1) {
+            result = "You Both Die";
+        } else if (playerOneHealth < 1) {
+            result = playerTwoName + " WINS!!!"
+        } else if (playerTwoHealth < 1) {
+            result = playerOneName + " WINS!!!"
+        };
+
+        return result;
+
+    };
+    /* the program begins */
+    console.log('program starts')
+    fight();
+
+})();
+
